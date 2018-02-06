0ad60d35f8247d4150fe86c531ac5b4c8929fff1
Lightbpk <ebKeir-+>
2018-02-06 15:04:00 +0700

    pre release server

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,40 +3,11 @@ import com.sample.tools.Tcpserver;
 import java.sql.*;
 
 public class Application {
+
     public static void main(String[] args) {
         Tcpserver tcpserver = new Tcpserver();
-        tcpserver.init(9001);
+        tcpserver.init();
         tcpserver.start();
-        /*try {
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            System.out.println("driver ok");
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-            System.out.println("!");
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-            System.out.println("!");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("!");
-        }
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/tcpservdb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
-                    "root",
-                    "toor");
-            System.out.println("connection ok");
-            Statement statement = connection.createStatement();
-            statement.execute("INSERT INTO tcpservdb.messages (thread, usr, message) Values (1,'usr','sometext'); ");
-            *//*ResultSet resultSet = statement.executeQuery("SELECT * FROM sandbox.tcp_messages");
-            while (resultSet.next()==true) {
-                System.out.println(resultSet.getString("message"));
-                System.out.println(resultSet.getString("id"));
-            }*//*
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("!");
-        }*/
     }
 }
 
--- src/main/java/com/sample/tools/Tcpserver.java
+++ src/main/java/com/sample/tools/Tcpserver.java
@@ -3,6 +3,7 @@ package com.sample.tools;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -10,14 +11,20 @@ import java.util.concurrent.Executors;
 //---------------------------Lisning 127.0.0.1 port portnum ----------------
 //--------------------------------------------------------------------------
 public class Tcpserver {
-
+    public static final String PATH_TO_PROPERTIES = "src/main/resources/server.properties";
+    FileInputStream fileInputStream;
     private ServerSocket serverSocket;
     private Socket socket = null;
     private int i=0;
     private ExecutorService exec = Executors.newCachedThreadPool();
-    public void init(int portnum) {
-        System.out.println("lisening 127.0.0.1 port " + portnum);
+    Properties prop = new Properties();
+    public void init() {
+        int portnum;
         try {
+            fileInputStream = new FileInputStream(PATH_TO_PROPERTIES);
+            prop.load(fileInputStream);
+            portnum = Integer.parseInt(prop.getProperty("port"));
+            System.out.println("lisening 127.0.0.1 port " + portnum);
             serverSocket = new ServerSocket(portnum);
         } catch (IOException e) {
             e.printStackTrace();
